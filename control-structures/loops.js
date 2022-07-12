5ad7c73e074f18c8f022297b7ca4abcba6173cc1
kaaf7 <amer.m.karim@gmail.com>
2022-07-12 16:54:42 +0200

    init

--- control-structures/loops.js
+++ control-structures/loops.js
@@ -1,3 +1,25 @@
-for (let i = 0; i < 10; i++){
-console.log(i);
+ for (let i = 0; i < 10; i++){
+ console.log(i);
+ }
+ 
+   const users =['Karim','Max','Michael'];
+    for ( const user of users ){
+        
+        console.log(user);
+    }
+     
+    const newUsers =['Karim','Max','Amer'];
+for (let j=0; j<newUsers.length; j++){
+
+    console.log(newUsers[j]);
+}
+
+const loggedInUsers = {
+    name: 'Karim', age: 32 ,isAdmin: true
+};
+
+for( const propName in loggedInUsers){
+
+    //console.log(propName);
+    console.log(loggedInUsers[propName]);
 }
