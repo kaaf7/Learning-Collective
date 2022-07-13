290ca210894cae4f1a776fc1fa108404611bced7
kaaf7 <amer.m.karim@gmail.com>
2022-07-13 12:46:05 +0200

    init

--- loops-exercise/loops-in-action/loops-in-action.js
+++ loops-exercise/loops-in-action/loops-in-action.js
@@ -23,6 +23,8 @@ calculateSumButton.addEventListener('click', calculateSum);
  for( const anchorElement of anchorElements){
  anchorElement.classList.add('highlight');
 }
+
+
 };
  highlightButtonSelector.addEventListener('click', highLightLinks);
 
@@ -42,10 +44,35 @@ calculateSumButton.addEventListener('click', calculateSum);
  }
 
 const rollTheDiceButton = document.querySelector('#statistics button');
-function rollDice (){}
+function rollDice (){
+return (Math.random() * 6) + 1;
+}
 function deriveNumberOfDiceRolls()
 {
     const targetNumberInputElement = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
     const enteredNumber = targetNumberInputElement.value;
+    diceRollsListElement.innerHTML='';
+    let hasRolledtargetNumbe= false;
+    let numberOfRolls = 0;
+
+    while (!hasRolledtargetNumbe){
+      const rollednumber =  rollDice();
+    //   if(rolledNumber == enteredNumber) {
+    //     hasRolledtargetNumbe=true;
+    //   }
+    numberOfRolls++; 
+    const newRollListitemElement = document.createElement('li');
+    const outPutText = 'Roll' + numberOfRolls + ': ' + rollednumber;
+    newRollListitemElement.textContent = outpuText;
+    diceRollsListElement.append(newRollListitemElement);
+    hasRolledtargetNumbe = rollednumber == enteredNumber;
+
+
+    }
+       const outputTotalRollsElement = document.getElementById('output-total-rolls')
+       const outputTargetNumberElement = document.getElementById('output-target-number')
+       outputTargetNumberElement.textContent = enteredNumber;
+       outputTotalRollsElement.textContent = numberOfRolls;
 }
 rollDiceButtonElement.addEventListener('click', deriveNumberOfDiceRolls);
