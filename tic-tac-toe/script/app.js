907c17f547b1e3f6a8fed8b7306f776ab0b423fc
kaaf7 <amer.m.karim@gmail.com>
2022-07-19 17:19:31 +0200

    init

--- tic-tac-toe/script/app.js
+++ tic-tac-toe/script/app.js
@@ -1,21 +1,25 @@
+let editedPlayer=0;
+const players=[ {name:'', symbol:'X', },{name:'', symbol:'O'} ];
+
+
 const playerConfigOverlayElement = document.getElementById('config-overlay') ;
 const backdropElement= document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorOutputElement =  document.getElementById('config-error');
 
 
-
-
-
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const confirmlConfigBtnElement = document.getElementById('confirm-config-btn');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+
 
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
-
-formElement.addEventListener('submit',savePlayerConfig);
+ // access to confirm Button in HTML through type submit and then adding an Eventlistener which is the function savePlayerConfig to save the data
+ formElement.addEventListener('submit',savePlayerConfig);
+ startNewGameBtnElement.addEventListener('click',)
