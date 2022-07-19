4543b56b257ae61a77aa998194ca4bcd663166d5
kaaf7 <amer.m.karim@gmail.com>
2022-07-19 19:32:44 +0200

    init

--- tic-tac-toe/script/app.js
+++ tic-tac-toe/script/app.js
@@ -1,11 +1,18 @@
-let editedPlayer=0;
+const gameData= [
+  [0, 0, 0]
+, [0, 0, 0], 
+  [0, 0, 0]];
+let editedPlayer= 0;
 const players=[ {name:'', symbol:'X', },{name:'', symbol:'O'} ];
-
+let activePlayer=0;
+let currentRound =1;
 
 const playerConfigOverlayElement = document.getElementById('config-overlay') ;
 const backdropElement= document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorOutputElement =  document.getElementById('config-error');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
@@ -14,7 +21,8 @@ const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const confirmlConfigBtnElement = document.getElementById('confirm-config-btn');
 const startNewGameBtnElement = document.getElementById('start-game-btn');
 
-
+// querySelectorall function selects all the array (li) children of the id gameboard in css for
+const gamFieldElements = document.querySelectorAll('#game-board li'); 
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -22,4 +30,10 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
  // access to confirm Button in HTML through type submit and then adding an Eventlistener which is the function savePlayerConfig to save the data
  formElement.addEventListener('submit',savePlayerConfig);
- startNewGameBtnElement.addEventListener('click',)
+ startNewGameBtnElement.addEventListener('click', startnewGame);
+//now going through the array so we can use each array element as individual separated element for X O Button
+
+for( const gamFieldElement of gamFieldElements){
+    gamFieldElement.addEventListener('click', selectGameField);
+
+ }
